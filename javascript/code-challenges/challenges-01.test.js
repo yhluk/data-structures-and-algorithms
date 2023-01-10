6300025de633c8b919abb982210e4556a6ee04d2
yurii hlukhyi <yhlukhyi@gmail.com>
2023-01-09 23:16:30 -0800

    stretch goal done

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -145,9 +145,27 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  
-};
-
+  let outputArray = [];
+  arr.forEach(element => {
+    if (element % 3 === 0 && element % 5 === 0)
+    {
+      outputArray.push('Fizz Buzz');
+    }
+    else if (element % 3 === 0)
+    {
+      outputArray.push('Fizz');
+    }
+    else if (element % 5 === 0)
+    {
+      outputArray.push('Buzz');
+    }
+    else
+    {
+      outputArray.push(element);
+    }
+  });
+  return outputArray;
+}
 /* ------------------------------------------------------------------------------------------------
 TESTS
 
@@ -199,7 +217,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
